4154499ad497bfcb2fa47634fca99068b4ebcf4d
mbed Cloud (Circle CI) <monty-bot@arm.com>
2017-09-05 12:17:50 +0000

    Add generated changes (2017-09-05 12:17:50.375997)

--- backends/statistics/src/main/java/com/arm/mbed/cloud/sdk/internal/statistics/api/StatisticsApi.java
+++ backends/statistics/src/main/java/com/arm/mbed/cloud/sdk/internal/statistics/api/StatisticsApi.java
@@ -23,7 +23,6 @@ public interface StatisticsApi {
    * This REST API is used to get account-specific statistics.
    * @param include A comma-separated list of requested metrics and total_count (if included, the response will contain total_count to specify the total number of records available). Supported values are:  - &#x60;transactions&#x60; - &#x60;full_registrations&#x60; - &#x60;registration_updates&#x60; - &#x60;deleted_registrations&#x60; - &#x60;expired_registrations&#x60; - &#x60;bootstraps_successful&#x60; - &#x60;bootstraps_failed&#x60; - &#x60;bootstraps_pending&#x60; - &#x60;handshakes_successful&#x60; - &#x60;device_server_rest_api_success&#x60; - &#x60;device_server_rest_api_error&#x60; - &#x60;total_count&#x60;  (required)
    * @param interval Group the data by this interval in minutes, hours, days or weeks. Sample values: 5m, 2h, 3d, 4w. The maximum interval cannot exceed one year (365 days). The allowed ranges are 5m-525600m/1h-8760h/1d-365d/1w-53w.  (required)
-   * @param authorization Bearer {Access Token}. A valid API Gateway access token. The token is validated and the associated account identifier is used to retrieve account-specific statistics.  (required)
    * @param start UTC time/year/date in RFC3339 format. Fetch the data with timestamp greater than or equal to this value. Sample values: 20170207T092056990Z / 2017-02-07T09:20:56.990Z / 2017 / 20170207. The maximum time between start and end parameters cannot exceed more than one year (365 days). The parameter is not mandatory, if the period is specified.  (optional)
    * @param end UTC time/year/date in RFC3339 format. Fetch the data with timestamp less than this value.Sample values: 20170207T092056990Z / 2017-02-07T09:20:56.990Z / 2017 / 20170207. The maximum time between start and end parameters cannot exceed more than one year ( 365 days ). The parameter is not mandatory, if the period is specified.  (optional)
    * @param period Period. Fetch the data for the period in minutes, hours, days or weeks. Sample values: 5m, 2h, 3d, 4w. The parameter is not mandatory, if the start and end time are specified. The maximum period cannot exceed one year (365 days). The allowed ranges are 5m-525600m/1h-8760h/1d-365d/1w-53w.  (optional)
@@ -35,7 +34,7 @@ public interface StatisticsApi {
   
   @GET("v3/metrics")
   Call<SuccessfulResponse> v3MetricsGet(
-    @retrofit2.http.Query("include") String include, @retrofit2.http.Query("interval") String interval, @retrofit2.http.Header("Authorization") String authorization, @retrofit2.http.Query("start") String start, @retrofit2.http.Query("end") String end, @retrofit2.http.Query("period") String period, @retrofit2.http.Query("limit") Integer limit, @retrofit2.http.Query("after") String after, @retrofit2.http.Query("order") String order
+    @retrofit2.http.Query("include") String include, @retrofit2.http.Query("interval") String interval, @retrofit2.http.Query("start") String start, @retrofit2.http.Query("end") String end, @retrofit2.http.Query("period") String period, @retrofit2.http.Query("limit") Integer limit, @retrofit2.http.Query("after") String after, @retrofit2.http.Query("order") String order
   );
 
 }
