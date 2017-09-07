e432bf4c0a0132edee980c59e6c8bba7bf3d977b
Adrien CABARBAYE <adrien.cabarbaye@arm.com>
2017-09-07 12:47:49 +0100

    Added new metrics fields

--- src/main/java/com/arm/mbed/cloud/sdk/connect/model/Metric.java
+++ src/main/java/com/arm/mbed/cloud/sdk/connect/model/Metric.java
@@ -3,6 +3,7 @@ package com.arm.mbed.cloud.sdk.connect.model;
 import java.util.Date;
 
 import com.arm.mbed.cloud.sdk.annotations.DefaultValue;
+import com.arm.mbed.cloud.sdk.annotations.Internal;
 import com.arm.mbed.cloud.sdk.annotations.Preamble;
 import com.arm.mbed.cloud.sdk.common.SDKModel;
 
@@ -77,10 +78,48 @@ public class Metric implements SDKModel {
      */
     @DefaultValue(value = "0")
     private final long deletedRegistrations;
+    /**
+     * Number of successful proxy requests from Mbed Cloud Connect to devices linked to the account. The proxy requests
+     * are made from Mbed Cloud Connect to devices when you try to read or write values to device resources using
+     * Connect API endpoints.
+     */
+    @DefaultValue(value = "0")
+    private final long successfulDeviceProxyRequests;
+    /**
+     * Number of failed proxy requests from Mbed Cloud Connect to devices linked to the account. The proxy requests are
+     * made from Mbed Cloud Connect to devices when you try to read or write values to device resources using Connect
+     * API endpoints.
+     */
+    @DefaultValue(value = "0")
+    private final long failedDeviceProxyRequests;
+    /**
+     * Number of successful subscription requests from Mbed Cloud Connect to devices linked to the account.The
+     * subscription requests are made from Mbed Cloud Connect to devices when you try to subscribe to a resource path
+     * using Connect API endpoints.
+     */
+    @DefaultValue(value = "0")
+    private final long successfulDeviceSubscriptionRequests;
+    /**
+     * Number of failed subscription requests from Mbed Cloud Connect to devices linked to the account. The subscription
+     * requests are made from Mbed Cloud Connect to devices when you try to subscribe to a resource path using Connect
+     * API endpoints.
+     */
+    @DefaultValue(value = "0")
+    private final long failedDeviceSubscriptionRequests;
+    /**
+     * Number of observations received by Mbed Cloud Connect from the devices linked to the account.The observations are
+     * pushed from the device to Mbed Cloud Connect when you have successfully subscribed to the device resources using
+     * Connect API endpoints.
+     */
+    @DefaultValue(value = "0")
+    private final long deviceObservations;
 
+    @Internal
     public Metric(String id, Date timestamp, long transactions, long successfulApiCalls, long failedApiCalls,
             long successfulHandshakes, long pendingBootstraps, long successfulBootstraps, long failedBootstraps,
-            long registrations, long updatedRegistrations, long expiredRegistrations, long deletedRegistrations) {
+            long registrations, long updatedRegistrations, long expiredRegistrations, long deletedRegistrations,
+            long successfulDeviceProxyRequests, long failedDeviceProxyRequests,
+            long successfulDeviceSubscriptionRequests, long failedDeviceSubscriptionRequests, long deviceObservations) {
         super();
         setId(id);
         this.timestamp = timestamp;
@@ -95,10 +134,15 @@ public class Metric implements SDKModel {
         this.updatedRegistrations = updatedRegistrations;
         this.expiredRegistrations = expiredRegistrations;
         this.deletedRegistrations = deletedRegistrations;
+        this.successfulDeviceProxyRequests = successfulDeviceProxyRequests;
+        this.failedDeviceProxyRequests = failedDeviceProxyRequests;
+        this.successfulDeviceSubscriptionRequests = successfulDeviceSubscriptionRequests;
+        this.failedDeviceSubscriptionRequests = failedDeviceSubscriptionRequests;
+        this.deviceObservations = deviceObservations;
     }
 
     public Metric() {
-        this(null, new Date(), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+        this(null, new Date(), 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
     }
 
     /**
@@ -198,6 +242,41 @@ public class Metric implements SDKModel {
      */
     public long getSuccessfulHandshakes() {
         return successfulHandshakes;
+    }
+
+    /**
+     * @return the successfulDeviceProxyRequests
+     */
+    public long getSuccessfulDeviceProxyRequests() {
+        return successfulDeviceProxyRequests;
+    }
+
+    /**
+     * @return the failedDeviceProxyRequests
+     */
+    public long getFailedDeviceProxyRequests() {
+        return failedDeviceProxyRequests;
+    }
+
+    /**
+     * @return the successfulDeviceSubscriptionRequests
+     */
+    public long getSuccessfulDeviceSubscriptionRequests() {
+        return successfulDeviceSubscriptionRequests;
+    }
+
+    /**
+     * @return the failedDeviceSubscriptionRequests
+     */
+    public long getFailedDeviceSubscriptionRequests() {
+        return failedDeviceSubscriptionRequests;
+    }
+
+    /**
+     * @return the deviceObservations
+     */
+    public long getDeviceObservations() {
+        return deviceObservations;
     }
 
 }
