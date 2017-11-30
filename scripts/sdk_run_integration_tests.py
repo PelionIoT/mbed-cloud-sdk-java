4d712617ea85aaf2be392394ea14edd4eb4afa7d
Adrien CABARBAYE <adrien.cabarbaye@arm.com>
2017-11-30 14:57:21 +0000

    Forced rebuild of test server docker image.

--- scripts/sdk_run_integration_tests.py
+++ scripts/sdk_run_integration_tests.py
@@ -20,7 +20,7 @@ class SDKIntegrationTestRunner(sdk_common.BuildStepUsingGradle):
             # return_code = self.call_command(arguments, None, True)
             # if return_code != 0:
             #     raise Exception('Error code', return_code)
-            arguments = ["docker-compose", "up", "--exit-code-from", "testrunner"]
+            arguments = ["docker-compose", "up", "--build", "--exit-code-from", "testrunner"]
             self.log_info("Running integration tests against integration lab")
             env = self.common_config.get_config().get_environment_with_host_set(self.host)
             env = self.common_config.get_config().get_environment_with_apikey_set(self.key_lab, env)
