2542f1ef1fb1f1dd13584b20842a33ec7d9c7e0b
Adrien CABARBAYE <adrien.cabarbaye@arm.com>
2017-12-05 14:55:37 +0000

    Modified docker compose actions

--- scripts/sdk_run_integration_tests.py
+++ scripts/sdk_run_integration_tests.py
@@ -25,16 +25,15 @@ class SDKIntegrationTestRunner(sdk_common.BuildStepUsingGradle):
             self.log_info("Running integration tests against integration lab")
             env = self.common_config.get_config().get_environment_with_host_set(self.host)
             env = self.common_config.get_config().get_environment_with_apikey_set(self.key_lab, env)
-            print(env)
             return_code_int = self.call_command(arguments, None, True, True, env)
             if return_code_int != 0:
                 self.log_warning("Failures have happened in Integration")
-            self.log_info("Running integration tests against production")
             env = self.common_config.get_config().get_environment_with_host_set("https://api.us-east-1.mbedcloud.com")
             env = self.common_config.get_config().get_environment_with_apikey_set(self.key_prod, env)
-            print(env)
             self.log_info("Restarting containers to take into account environment changes")
+            self.call_command(["docker-compose", "down"], None, True, True, env)
             self.call_command(["docker-compose", "restart"], None, True, True, env)
+            self.log_info("Running integration tests against production")
             return_code_prod = self.call_command(arguments, None, True, True, env)
             if return_code_prod != 0:
                 self.log_warning("Failures have happened in Production")
