0ed0f80d0ab08e1b78f61011c8e12c2c15b021b2
Adrien CABARBAYE <adrien.cabarbaye@arm.com>
2018-02-23 13:14:29 +0000

    more

--- src/main/java/com/arm/mbed/cloud/sdk/subscribe/AbstractObserver.java
+++ src/main/java/com/arm/mbed/cloud/sdk/subscribe/AbstractObserver.java
@@ -1,5 +1,68 @@
 package com.arm.mbed.cloud.sdk.subscribe;
 
-public class AbstractObserver implements Observer<T> {
+import java.util.Iterator;
+import java.util.concurrent.Future;
+
+import io.reactivex.Flowable;
+
+public class AbstractObserver<T> implements Observer<T> {
+
+    private final Flowable<T> flow;
+
+    // private final
+
+    @Override
+    public boolean hasNext() {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    public Future<T> next() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public Iterator<Future<T>> iterator() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void addCallback(NotificationCallback<T> callback) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void removeCallback() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void unsubscribe() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public Flowable<T> flow() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public T one() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public boolean isCancelled() {
+        // TODO Auto-generated method stub
+        return false;
+    }
 
 }
