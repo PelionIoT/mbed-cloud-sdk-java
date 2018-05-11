422a63ebf3d719566ad962b13bc2cc3fe9dcdda8
mbed Cloud (Circle CI) <monty-bot@arm.com>
2018-05-11 12:02:05 +0000

    mds via codegen @ 2018-05-11 12:02

--- backends/mds/src/main/java/com/arm/mbed/cloud/sdk/internal/mds/api/NotificationsApi.java
+++ backends/mds/src/main/java/com/arm/mbed/cloud/sdk/internal/mds/api/NotificationsApi.java
@@ -19,12 +19,21 @@ import java.util.Map;
 
 public interface NotificationsApi {
   /**
+   * Delete notification Long Poll channel
+   * To delete a notification Long Poll channel. This is required to change the channel from Long Poll to a callback. You should not make a GET &#x60;/v2/notification/pull&#x60; call for 2 minutes after channel was deleted, because it can implicitly recreate the pull channel. You can also have some random responses with payload or 410 GONE with \&quot;CHANNEL_DELETED\&quot; as a payload or 200/204 until the old channel is purged.  **Example usage:**     curl -X DELETE https://api.us-east-1.mbedcloud.com/v2/notification/pull -H &#39;authorization: Bearer {api-key}&#39; 
+   * @return Call&lt;Void&gt;
+   */
+  @DELETE("v2/notification/pull")
+  Call<Void> deleteLongPollChannel();
+    
+
+  /**
    * Delete callback URL
    * Deletes the callback URL.  **Example usage:**      curl -X DELETE https://api.us-east-1.mbedcloud.com/v2/notification/callback -H &#39;authorization: Bearer {api-key}&#39; 
    * @return Call&lt;Void&gt;
    */
   @DELETE("v2/notification/callback")
-  Call<Void> v2NotificationCallbackDelete();
+  Call<Void> deregisterWebhook();
     
 
   /**
@@ -33,7 +42,16 @@ public interface NotificationsApi {
    * @return Call&lt;Webhook&gt;
    */
   @GET("v2/notification/callback")
-  Call<Webhook> v2NotificationCallbackGet();
+  Call<Webhook> getWebhook();
+    
+
+  /**
+   * Get notifications using Long Poll
+   * In this case, notifications are delivered through HTTP long poll requests. The HTTP request is kept open until an event notification or a batch of event notifications are delivered to the client or the request times out (response code 204). In both cases, the client should open a new polling connection after the previous one closes. Only a single long polling connection per API key can be ongoing at any given time. You must have a persistent connection (Connection keep-alive header in the request) to avoid excess TLS handshakes.  The pull channel is implicitly created by the first GET call to &#x60;/v2/notification/pull&#x60;. It is refreshed on each GET call. If the channel is not polled for a long time (10 minutes) - it expires and will be deleted. This means that no notifications will stay in the queue between polls. A channel can be also deleted explicitly by a DELETE call.  **Note:** If you cannot have a public facing callback URL, for example when developing on your local machine, you can use long polling to check for new messages. However, **long polling is deprecated** and will likely be replaced in future. It is meant only for experimentation and not for commercial usage. The proper method to receive notifications is a **notification callback**. There can only be one notification channel per API key at a time in Mbed Cloud Connect. If a callback notification channel already exists, you need to delete it before creating a long poll notification channel, and vice-versa.  **Example usage:**      curl -X GET https://api.us-east-1.mbedcloud.com/v2/notification/pull -H &#39;authorization: Bearer {api-key}&#39; 
+   * @return Call&lt;NotificationMessage&gt;
+   */
+  @GET("v2/notification/pull")
+  Call<NotificationMessage> longPollNotifications();
     
 
   /**
@@ -46,26 +64,8 @@ public interface NotificationsApi {
     "Content-Type:application/json"
   })
   @PUT("v2/notification/callback")
-  Call<Void> v2NotificationCallbackPut(
+  Call<Void> registerWebhook(
     @retrofit2.http.Body Webhook webhook
   );
-
-  /**
-   * Delete notification Long Poll channel
-   * To delete a notification Long Poll channel. This is required to change the channel from Long Poll to a callback. You should not make a GET &#x60;/v2/notification/pull&#x60; call for 2 minutes after channel was deleted, because it can implicitly recreate the pull channel. You can also have some random responses with payload or 410 GONE with \&quot;CHANNEL_DELETED\&quot; as a payload or 200/204 until the old channel is purged.  **Example usage:**     curl -X DELETE https://api.us-east-1.mbedcloud.com/v2/notification/pull -H &#39;authorization: Bearer {api-key}&#39; 
-   * @return Call&lt;Void&gt;
-   */
-  @DELETE("v2/notification/pull")
-  Call<Void> v2NotificationPullDelete();
-    
-
-  /**
-   * Get notifications using Long Poll
-   * In this case, notifications are delivered through HTTP long poll requests. The HTTP request is kept open until an event notification or a batch of event notifications are delivered to the client or the request times out (response code 204). In both cases, the client should open a new polling connection after the previous one closes. Only a single long polling connection per API key can be ongoing at any given time. You must have a persistent connection (Connection keep-alive header in the request) to avoid excess TLS handshakes.  The pull channel is implicitly created by the first GET call to &#x60;/v2/notification/pull&#x60;. It is refreshed on each GET call. If the channel is not polled for a long time (10 minutes) - it expires and will be deleted. This means that no notifications will stay in the queue between polls. A channel can be also deleted explicitly by a DELETE call.  **Note:** If you cannot have a public facing callback URL, for example when developing on your local machine, you can use long polling to check for new messages. However, **long polling is deprecated** and will likely be replaced in future. It is meant only for experimentation and not for commercial usage. The proper method to receive notifications is a **notification callback**. There can only be one notification channel per API key at a time in Mbed Cloud Connect. If a callback notification channel already exists, you need to delete it before creating a long poll notification channel, and vice-versa.  **Example usage:**      curl -X GET https://api.us-east-1.mbedcloud.com/v2/notification/pull -H &#39;authorization: Bearer {api-key}&#39; 
-   * @return Call&lt;NotificationMessage&gt;
-   */
-  @GET("v2/notification/pull")
-  Call<NotificationMessage> v2NotificationPullGet();
-    
 
 }
--- backends/mds/src/test/java/com/arm/mbed/cloud/sdk/internal/mds/api/SubscriptionsApiTest.java
+++ backends/mds/src/test/java/com/arm/mbed/cloud/sdk/internal/mds/api/SubscriptionsApiTest.java
@@ -24,50 +24,15 @@ public class SubscriptionsApiTest {
     }
 
     /**
-     * Remove pre-subscriptions
+     * Subscribe to a resource path
      *
-     * Removes pre-subscriptions.  **Example usage:**      curl -X DELETE https://api.us-east-1.mbedcloud.com/v2/subscriptions -H &#39;authorization: Bearer {api-key}&#39; 
+     * The Mbed Cloud Connect eventing model consists of observable resources.  This means that endpoints can deliver updated resource content, periodically or with a more sophisticated solution-dependent logic. The OMA LwM2M resource model including objects, object instances, resources and resource instances is also supported.  Applications can subscribe to objects, object instances or individual resources to make the device to provide value change notifications to Mbed Cloud Connect service. An application needs to call a &#x60;/notification/callback&#x60; method to get Mbed Cloud Connect to push notifications of the resource changes.  **Notification rules**  A web application can place dynamic observation rules for individual Object Instances and Resources to define when the device sends observations. More information in [Notification rules](/docs/current/connecting/resource-change-webapp.html).  All manual subscriptions are removed during a full device registration and applications need to re-subscribe at that point. To avoid this, you can use &#x60;/subscriptions&#x60; to set a pre-subscription.  **Example usage:**      curl -X PUT \\       https://api.us-east-1.mbedcloud.com/v2/subscriptions/{device-id}/{resourcePath} \\       -H &#39;authorization: Bearer {api-key}&#39; 
      */
     @Test
-    public void v2SubscriptionsDeleteTest() {
-        // Void response = api.v2SubscriptionsDelete();
-
-        // TODO: test validations
-    }
-    /**
-     * Delete subscriptions from an endpoint
-     *
-     * Deletes all resource subscriptions in a single endpoint.  **Example usage:**      curl -X DELETE \\       https://api.us-east-1.mbedcloud.com/v2/subscriptions/{device-id} \\       -H &#39;authorization: Bearer {api-key}&#39; 
-     */
-    @Test
-    public void v2SubscriptionsDeviceIdDeleteTest() {
-        String deviceId = null;
-        // Void response = api.v2SubscriptionsDeviceIdDelete(deviceId);
-
-        // TODO: test validations
-    }
-    /**
-     * Read endpoints subscriptions
-     *
-     * Lists all subscribed resources from a single endpoint.  **Example usage:**      curl -X GET \\       https://api.us-east-1.mbedcloud.com/v2/subscriptions/{device-id} \\       -H &#39;authorization: Bearer {api-key}&#39; 
-     */
-    @Test
-    public void v2SubscriptionsDeviceIdGetTest() {
-        String deviceId = null;
-        // String response = api.v2SubscriptionsDeviceIdGet(deviceId);
-
-        // TODO: test validations
-    }
-    /**
-     * Remove a subscription
-     *
-     * To remove an existing subscription from a resource path.  **Example usage:**      curl -X DELETE \\       https://api.us-east-1.mbedcloud.com/v2/subscriptions/{device-id}/{resourcePath} \\       -H &#39;authorization: Bearer {api-key}&#39; 
-     */
-    @Test
-    public void v2SubscriptionsDeviceIdResourcePathDeleteTest() {
+    public void addResourceSubscriptionTest() {
         String deviceId = null;
         String resourcePath = null;
-        // Void response = api.v2SubscriptionsDeviceIdResourcePathDelete(deviceId, resourcePath);
+        // Void response = api.addResourceSubscription(deviceId, resourcePath);
 
         // TODO: test validations
     }
@@ -77,23 +42,58 @@ public class SubscriptionsApiTest {
      * 
      */
     @Test
-    public void v2SubscriptionsDeviceIdResourcePathGetTest() {
+    public void checkResourceSubscriptionTest() {
         String deviceId = null;
         String resourcePath = null;
-        // Void response = api.v2SubscriptionsDeviceIdResourcePathGet(deviceId, resourcePath);
+        // Void response = api.checkResourceSubscription(deviceId, resourcePath);
 
         // TODO: test validations
     }
     /**
-     * Subscribe to a resource path
+     * Delete subscriptions from an endpoint
      *
-     * The Mbed Cloud Connect eventing model consists of observable resources.  This means that endpoints can deliver updated resource content, periodically or with a more sophisticated solution-dependent logic. The OMA LwM2M resource model including objects, object instances, resources and resource instances is also supported.  Applications can subscribe to objects, object instances or individual resources to make the device to provide value change notifications to Mbed Cloud Connect service. An application needs to call a &#x60;/notification/callback&#x60; method to get Mbed Cloud Connect to push notifications of the resource changes.  **Notification rules**  A web application can place dynamic observation rules for individual Object Instances and Resources to define when the device sends observations. More information in [Notification rules](/docs/current/connecting/resource-change-webapp.html).  All manual subscriptions are removed during a full device registration and applications need to re-subscribe at that point. To avoid this, you can use &#x60;/subscriptions&#x60; to set a pre-subscription.  **Example usage:**      curl -X PUT \\       https://api.us-east-1.mbedcloud.com/v2/subscriptions/{device-id}/{resourcePath} \\       -H &#39;authorization: Bearer {api-key}&#39; 
+     * Deletes all resource subscriptions in a single endpoint.  **Example usage:**      curl -X DELETE \\       https://api.us-east-1.mbedcloud.com/v2/subscriptions/{device-id} \\       -H &#39;authorization: Bearer {api-key}&#39; 
      */
     @Test
-    public void v2SubscriptionsDeviceIdResourcePathPutTest() {
+    public void deleteEndpointSubscriptionsTest() {
+        String deviceId = null;
+        // Void response = api.deleteEndpointSubscriptions(deviceId);
+
+        // TODO: test validations
+    }
+    /**
+     * Remove pre-subscriptions
+     *
+     * Removes pre-subscriptions.  **Example usage:**      curl -X DELETE https://api.us-east-1.mbedcloud.com/v2/subscriptions -H &#39;authorization: Bearer {api-key}&#39; 
+     */
+    @Test
+    public void deletePreSubscriptionsTest() {
+        // Void response = api.deletePreSubscriptions();
+
+        // TODO: test validations
+    }
+    /**
+     * Remove a subscription
+     *
+     * To remove an existing subscription from a resource path.  **Example usage:**      curl -X DELETE \\       https://api.us-east-1.mbedcloud.com/v2/subscriptions/{device-id}/{resourcePath} \\       -H &#39;authorization: Bearer {api-key}&#39; 
+     */
+    @Test
+    public void deleteResourceSubscriptionTest() {
         String deviceId = null;
         String resourcePath = null;
-        // Void response = api.v2SubscriptionsDeviceIdResourcePathPut(deviceId, resourcePath);
+        // Void response = api.deleteResourceSubscription(deviceId, resourcePath);
+
+        // TODO: test validations
+    }
+    /**
+     * Read endpoints subscriptions
+     *
+     * Lists all subscribed resources from a single endpoint.  **Example usage:**      curl -X GET \\       https://api.us-east-1.mbedcloud.com/v2/subscriptions/{device-id} \\       -H &#39;authorization: Bearer {api-key}&#39; 
+     */
+    @Test
+    public void getEndpointSubscriptionsTest() {
+        String deviceId = null;
+        // String response = api.getEndpointSubscriptions(deviceId);
 
         // TODO: test validations
     }
@@ -103,8 +103,8 @@ public class SubscriptionsApiTest {
      * You can retrieve the pre-subscription data with the GET operation. The server returns with the same JSON structure as described above. If there are no pre-subscribed resources, it returns with an empty array.  **Example usage:**      curl -X GET https://api.us-east-1.mbedcloud.com/v2/subscriptions -H &#39;authorization: Bearer {api-key}&#39; 
      */
     @Test
-    public void v2SubscriptionsGetTest() {
-        // PresubscriptionArray response = api.v2SubscriptionsGet();
+    public void getPreSubscriptionsTest() {
+        // PresubscriptionArray response = api.getPreSubscriptions();
 
         // TODO: test validations
     }
@@ -114,9 +114,9 @@ public class SubscriptionsApiTest {
      * Pre-subscription is a set of rules and patterns put by the application. When an endpoint registers and its ID, type and registered resources match the pre-subscription data, Mbed Cloud Connect sends subscription requests to the device automatically. The pattern may include the endpoint ID (optionally having an &#x60;*&#x60; character at the end), endpoint type, a list of resources or expressions with an &#x60;*&#x60; character at the end. Subscriptions based on pre-subscriptions are done when device registers or does register update. To remove the pre-subscription data, put an empty array as a rule.  **Notification rules**  A web application can place dynamic observation rules for individual Object Instances and Resources to define when the device sends observations. More information in [Notification rules](/docs/current/connecting/resource-change-webapp.html).  **Limits**:  - The maximum length of the endpoint name and endpoint type is 64 characters. - The maximum length of the resource path is 128 characters. - You can listen to 256 separate resource paths. - The maximum number of pre-subscription entries is 1024.  **Example request:**  &#x60;&#x60;&#x60; curl -X PUT \\   https://api.us-east-1.mbedcloud.com/v2/subscriptions \\   -H &#39;authorization: Bearer {api-key}&#39; \\   -H &#39;content-type: application/json&#39; \\   -d &#39;[          {            \&quot;endpoint-name\&quot;: \&quot;node-001\&quot;,            \&quot;resource-path\&quot;: [\&quot;/dev\&quot;]          },          {            \&quot;endpoint-type\&quot;: \&quot;Light\&quot;,            \&quot;resource-path\&quot;: [\&quot;/sen/_*\&quot;]          },          {            \&quot;endpoint-name\&quot;: \&quot;node*\&quot;          },          {            \&quot;endpoint-type\&quot;: \&quot;Sensor\&quot;          },          {            \&quot;resource-path\&quot;: [\&quot;/dev/temp\&quot;,\&quot;/dev/hum\&quot;]          }       ]&#39; &#x60;&#x60;&#x60;  - Subscribe to &#x60;/dev&#x60; resource of endpoint named &#x60;node-001&#x60;. - Subscribe to &#x60;Light&#x60; type of endpoints and their resources prefixed with &#x60;/sen/&#x60;. - Subscribe to all observable resources of endpoint names prefixed with &#x60;node&#x60;. - Subscribe to all observable resources of &#x60;Sensor&#x60; type endpoints. - Subscribe to &#x60;/dev/temp&#x60; and &#x60;/dev/hum&#x60; resources of all endpoints.  **Note**: For efficiency reasons, you should use resource path patterns in the pre-subscription data. This prevents the notification flow from unwanted resources. 
      */
     @Test
-    public void v2SubscriptionsPutTest() {
+    public void updatePreSubscriptionsTest() {
         PresubscriptionArray presubsription = null;
-        // Void response = api.v2SubscriptionsPut(presubsription);
+        // Void response = api.updatePreSubscriptions(presubsription);
 
         // TODO: test validations
     }
