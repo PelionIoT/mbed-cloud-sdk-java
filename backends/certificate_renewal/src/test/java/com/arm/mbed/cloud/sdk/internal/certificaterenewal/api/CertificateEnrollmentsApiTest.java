9adada916bc182bd29ae9dfa146ad80060e83eff
mbed Cloud (Circle CI) <monty-bot@arm.com>
2018-10-02 07:03:18 +0000

    certificate_renewal via codegen @ 2018-10-02 07:03

--- backends/certificate_renewal/src/test/java/com/arm/mbed/cloud/sdk/internal/certificaterenewal/api/CertificateEnrollmentsApiTest.java
+++ backends/certificate_renewal/src/test/java/com/arm/mbed/cloud/sdk/internal/certificaterenewal/api/CertificateEnrollmentsApiTest.java
@@ -39,12 +39,25 @@ public class CertificateEnrollmentsApiTest {
     /**
      * Get certificate enrollments list.
      *
-     * Get certificate enrollments, optionally filtered by device ID.  **Example usage:**  &#x60;&#x60;&#x60; curl -H &#39;authorization: Bearer &lt;valid access token&gt;&#39; https://api.us-east-1.mbedcloud.com/v3/certificate-enrollments &#x60;&#x60;&#x60; &#x60;&#x60;&#x60; curl -H &#39;authorization: Bearer &lt;valid access token&gt;&#39; https://api.us-east-1.mbedcloud.com/v3/certificate-enrollments?device_id__eq&#x3D;01612df56f3b0a580a010fc700000000 &#x60;&#x60;&#x60; 
+     *  Get certificate enrollments list, optionally filtered.  **Example usage:**  &#x60;&#x60;&#x60; curl -H &#39;authorization: Bearer &lt;valid access token&gt;&#39; https://api.us-east-1.mbedcloud.com/v3/certificate-enrollments &#x60;&#x60;&#x60; &#x60;&#x60;&#x60; curl -H &#39;authorization: Bearer &lt;valid access token&gt;&#39; https://api.us-east-1.mbedcloud.com/v3/certificate-enrollments?device_id__eq&#x3D;01612df56f3b0a580a010fc700000000 &#x60;&#x60;&#x60; 
      */
     @Test
     public void getCertificateEnrollmentsTest() {
         String deviceIdEq = null;
-        // CertificateEnrollmentListResponse response = api.getCertificateEnrollments(deviceIdEq);
+        String certificateNameEq = null;
+        Integer limit = null;
+        String after = null;
+        String order = null;
+        String include = null;
+        String enrollStatusNeq = null;
+        String enrollStatusEq = null;
+        String enrollResultNeq = null;
+        String enrollResultEq = null;
+        String createdAtLte = null;
+        String createdAtGte = null;
+        String updatedAtLte = null;
+        String updatedAtGte = null;
+        // CertificateEnrollmentListResponse response = api.getCertificateEnrollments(deviceIdEq, certificateNameEq, limit, after, order, include, enrollStatusNeq, enrollStatusEq, enrollResultNeq, enrollResultEq, createdAtLte, createdAtGte, updatedAtLte, updatedAtGte);
 
         // TODO: test validations
     }
