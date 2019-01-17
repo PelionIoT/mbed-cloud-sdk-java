16bf8458898045bd65642e5b4213423aa505392b
Adrien CABARBAYE <adrien.cabarbaye@arm.com>
2019-01-17 19:08:42 +0000

    testing the generation

--- scripts/sdk_fetch_foundation_generator_info.py
+++ scripts/sdk_fetch_foundation_generator_info.py
@@ -5,14 +5,14 @@ import sdk_common
 # Block in charge of getting information about the generator
 class SDKFoundationGeneratorInfo(sdk_common.BuildStepUsingGradle):
     def __init__(self, logger=None):
-        super(SDKFoundationGeneratorInfo, self).__init__('SDK foundation generator info', logger)
+        super(SDKFoundationGeneratorInfo, self).__init__('Foundation generator info', logger)
         self.gradle_directory = self.common_config.get_config().get_sdk_foundation_generation_directory()
         self.reset()
 
     def execute(self):
         self.print_title()
         try:
-            self.log_info("Generating the changelog")
+            self.log_info("Fetching key information about the foundation SDK generator")
             self.execute_gradle_task('info')
         except:
             self.log_error('Failed to get foundation generator info')
