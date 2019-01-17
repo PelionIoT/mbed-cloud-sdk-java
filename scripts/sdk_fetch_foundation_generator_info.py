4140cde6dc2584ac65ebb05c2435072677445b10
Adrien CABARBAYE <adrien.cabarbaye@arm.com>
2019-01-17 17:37:05 +0000

    Refactored slightly the python scripts and try to generate Foundation automatically

---
+++ scripts/sdk_fetch_foundation_generator_info.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python
+import sdk_common
+
+
+# Block in charge of getting information about the generator
+class SDKFoundationGeneratorInfo(sdk_common.BuildStepUsingGradle):
+    def __init__(self, logger=None):
+        super(SDKFoundationGeneratorInfo, self).__init__('SDK foundation generator info', logger)
+        self.gradle_directory = self.common_config.get_config().get_sdk_foundation_generation_directory()
+        self.reset()
+
+    def execute(self):
+        self.print_title()
+        try:
+            self.log_info("Generating the changelog")
+            self.execute_gradle_task('info')
+        except:
+            self.log_error('Failed to get foundation generator info')
+            return False
+        self.log_info("Done.")
+        return True
