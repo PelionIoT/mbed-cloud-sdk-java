4f085e4fcca7a8f79802ddddc2e29d8fa6f7e7c9
Adrien CABARBAYE <adrien.cabarbaye@arm.com>
2019-03-26 16:47:59 +0000

    Updated examples

--- examples/src/main/java/pelion_cloud_sdk/LegacyExamples.java
+++ examples/src/main/java/pelion_cloud_sdk/LegacyExamples.java
@@ -14,8 +14,25 @@ public class LegacyExamples extends AbstractExample {
      * Using the Billing APIs
      */
     @Example
-    public void getQuotaHistory() {
-        // an example: hello world legacy
+    public void getRemainingQuota() {
+        // an example: legacy get resource
+        try (Billing billingApi = new Billing(Configuration.get())) {
+            log("Quota remaining: " + billingApi.getQuotaRemaining());
+
+        } catch (MbedCloudException exception) {
+            // TODO do something with the exception
+            exception.printStackTrace();
+            // Fails if an exception happened
+            // cloak
+            fail(exception.getMessage());
+            // uncloak
+        }
+        // end of example
+    }
+
+    @Example
+    public void listQuotaHistory() {
+        // an example: legacy listing resources
         try (Billing billingApi = new Billing(Configuration.get())) {
             billingApi.getAllQuotaHistory(null)
                       .forEach(q -> log("Quota change reason: " + q.getReason() + ", delta: " + q.getDelta()));
